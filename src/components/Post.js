d58ee276b0fc3069d1a8602f912c5dacd2d0c49b
Iago Virgílio <iagovirgilio@gmail.com>
2020-04-08 10:42:46 -0300

    Criado component AddComment

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Dimensions, Image} from 'react-native';
 import Author from './Author';
 import Comments from './Comments';
+import AddComment from './AddComment';
 
 class Post extends Component {
   render() {
@@ -10,6 +11,7 @@ class Post extends Component {
         <Image source={this.props.image} style={styles.image} />
         <Author email="fulano@gmail.com" nickname="Fulano de Tal" />
         <Comments comments={this.props.comments} />
+        <AddComment />
       </View>
     );
   }
